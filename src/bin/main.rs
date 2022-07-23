bd64ad584fc9123de87c2778d6d9678e7a6be18e
turtletongue <platonsterh@gmail.com>
2022-07-23 23:53:29 +0500

    fix: remove derive Debug from heap

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,11 +1,5 @@
 use std::error::Error;
 
-use rust_data_structures::heaps::Heap;
-
 fn main() -> Result<(), Box<dyn Error>> {
-  let heap = Heap::heapify(vec![1, 6, 2, 5, 3, 4]);
-
-  println!("{:#?}", heap);
-
   Ok(())
 }
--- src/heaps.rs
+++ src/heaps.rs
@@ -4,7 +4,6 @@ enum HeapChild {
   Right,
 }
 
-#[derive(Debug)]
 pub struct Heap<T> {
   items: Vec<T>,
 }

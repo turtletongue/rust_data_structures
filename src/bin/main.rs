ab6576d94571b0cd346928da2c74ed0ba64c1e6b
turtletongue <platonsterh@gmail.com>
2022-07-28 16:35:09 +0500

    feat: implement dijkstra algorithm for WeightedGraph

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -5,19 +5,27 @@ use rust_data_structures::weighted_graphs::WeightedGraph;
 fn main() -> Result<(), Box<dyn Error>> {
   let mut graph = WeightedGraph::new();
 
-  let alice = "Alice";
-  let bob = "Bob";
-  let john = "John";
+  let a = "A";
+  let b = "B";
+  let c = "C";
+  let d = "D";
+  let e = "E";
 
-  graph.add_node(&alice);
-  graph.add_node(&bob);
-  graph.add_node(&john);
+  graph.add_node(&a);
+  graph.add_node(&b);
+  graph.add_node(&c);
+  graph.add_node(&d);
+  graph.add_node(&e);
 
-  graph.add_edge(&alice, &john, 1)?;
-  graph.add_edge(&bob, &john, 5)?;
-  graph.add_edge(&john, &bob, 7)?;
+  graph.add_edge(&a, &b, 1)?;
+  graph.add_edge(&a, &d, 5)?;
+  graph.add_edge(&a, &c, 1)?;
+  graph.add_edge(&b, &e, 4)?;
+  graph.add_edge(&d, &e, 1)?;
+  graph.add_edge(&c, &d, 1)?;
+  graph.add_edge(&b, &d, 4)?;
 
-  println!("{:#?}", graph);
+  println!("{:#?}", graph.shortest_path(&a, &e));
 
   Ok(())
 }
--- src/weighted_graphs.rs
+++ src/weighted_graphs.rs
@@ -1,6 +1,5 @@
-use std::{collections::HashMap, hash::Hash};
+use std::{collections::{HashMap, BinaryHeap, HashSet}, hash::Hash, fmt::Display, cmp::{Ordering, Reverse}};
 
-#[derive(Debug)]
 struct Edge {
   weight: usize,
 }
@@ -11,7 +10,6 @@ impl Edge {
   }
 }
 
-#[derive(Debug)]
 struct Node<'a, T> {
   value: &'a T,
   edges: HashMap<&'a T, Edge>,
@@ -29,14 +27,58 @@ impl<'a, T: Eq + Hash> Node<'a, T> {
     let edge = self.edges.entry(to).or_insert(Edge::new(weight));
     edge.weight = weight;
   }
+
 }
 
-#[derive(Debug)]
+impl<'a, T: Display> Display for Node<'a, T> {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    if self.edges.len() == 0 {
+      return Ok(());
+    }
+
+    let mut formatted_edges = String::new();
+
+    for (index, (value, edge)) in self.edges.iter().enumerate() {
+      let delimeter = if index == self.edges.len() - 1 { "" } else { ", " };
+
+      formatted_edges.push_str(&(format!("{}({}){}", value, edge.weight, delimeter)));
+    }
+
+    let formatted_node = format!("{} is connected with [{}]", self.value.to_string(), formatted_edges);
+
+    writeln!(f, "{}", formatted_node)?;
+
+    Ok(())
+  }
+}
+
+impl<'a, T: PartialEq> PartialEq for Node<'a, T> {
+  fn eq(&self, other: &Self) -> bool {
+    self.value == other.value
+  }
+}
+
+impl<'a, T: PartialOrd> PartialOrd for Node<'a, T> {
+  fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+    self.value.partial_cmp(&other.value)
+  }
+}
+
+impl<'a, T: Eq> Eq for Node<'a, T> {
+  fn assert_receiver_is_total_eq(&self) {}
+}
+
+impl<'a, T: Ord> Ord for Node<'a, T> {
+  fn cmp(&self, other: &Self) -> Ordering {
+    self.value.cmp(other.value)
+  }
+}
+
 pub struct WeightedGraph<'a, T> {
   nodes: HashMap<&'a T, Node<'a, T>>,
 }
 
-impl<'a, T: Eq + Hash> WeightedGraph<'a, T> {
+impl<'a, T: Eq + Hash + Ord> WeightedGraph<'a, T> {
   pub fn new() -> Self {
     Self {
       nodes: HashMap::new(),
@@ -62,7 +104,95 @@ impl<'a, T: Eq + Hash> WeightedGraph<'a, T> {
     Ok(())
   }
 
+  pub fn shortest_path(&self, from: &'a T, to: &'a T) -> Result<Vec<&'a T>, &'static str> {
+    let from_node = self.nodes.get(from);
+
+    if from_node.is_none() {
+      return Err("From node is not exist");
+    }
+
+    let to_node = self.nodes.get(to);
+
+    if to_node.is_none() {
+      return Err("To node is not exist");
+    }
+
+    let mut priority_queue = BinaryHeap::new();
+    let mut distances = HashMap::new();
+    let mut previous_nodes = HashMap::new(); 
+
+    priority_queue.push(Reverse(from_node.unwrap()));
+    distances.insert(from, 0);
+
+    let mut visited_nodes = HashSet::with_capacity(self.nodes.len());
+
+    while priority_queue.len() > 0 {
+      let node_entry = priority_queue.pop();
+
+      if node_entry.is_none() {
+        continue;
+      }
+
+      let node = node_entry.unwrap().0;
+
+      for (current, edge) in &node.edges {
+        if visited_nodes.contains(current) {
+          continue;
+        }
+
+        let proposed_distance = edge.weight + distances.get(node.value).or(Some(&0)).unwrap();
+        let current_distance = distances.entry(current).or_insert(proposed_distance);
+    
+        if proposed_distance > *current_distance {
+          continue;
+        }
+    
+        *current_distance = proposed_distance;
+        
+        previous_nodes.insert(current, node.value);
+        priority_queue.push(Reverse(&self.nodes[current]));
+      }
+
+      visited_nodes.insert(node.value);
+    }
+
+    Ok(self.construct_path(to_node, previous_nodes))
+  }
+
+  fn construct_path(&self, to_node: Option<&Node<'a, T>>, previous_nodes: HashMap<&&'a T, &'a T>) -> Vec<&'a T> {
+    let mut result = Vec::new();
+    let mut current = to_node;
+
+    while current.is_some() {
+      result.push(current.unwrap().value);
+
+      let previous = previous_nodes.get(&current.unwrap().value);
+
+      current = if let Some(previous) = previous {
+        Some(&self.nodes[previous])
+      } else {
+        None
+      }
+    }
+    
+    result.iter().map(|value| *value).rev().collect()
+  }
+
   fn is_node_exists(&self, value: &T) -> bool {
     self.nodes.contains_key(value)
+  }
+}
+
+impl<'a, T: Eq + Hash + Display> Display for WeightedGraph<'a, T> {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    let mut formatted_graph = String::new();
+
+    for node in self.nodes.values() {
+      formatted_graph.push_str(&node.to_string());
+    }
+
+    write!(f, "{}", formatted_graph)?;
+    
+    Ok(())
   }
 }
